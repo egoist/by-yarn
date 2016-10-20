38dca148fb527ae439915a91fb08319552b0bd23
EGOIST <0x142857@gmail.com>
2016-10-20 12:44:57 +0800

    fix test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,7 +2,7 @@ const byYarn = require('../')
 
 test('main', () => {
   if (process.env.CI) {
-    expect(byYarn).toBe(false)
+    expect(byYarn()).toBe(false)
   } else {
     expect(byYarn()).toBe(true)
   }
