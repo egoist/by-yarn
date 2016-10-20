6cfd66e0deb5ac0f682cd53a352fbd19427eea44
EGOIST <0x142857@gmail.com>
2016-10-20 22:56:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 const path = require('path')
 const pathExists = require('path-exists')
 
-delete require.cache[__filename];
-const parentDir = path.dirname(module.parent.filename);
-
 module.exports = function (cwd) {
   return __dirname.indexOf('.yarn-config/global') !== -1 || // global
     (
